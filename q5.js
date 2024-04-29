83bf44e110e0d47f5ff6908a550e25efd09ba82e
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 10:06:13 -0700

    Q 05 ts/ js node js of 45 assignment
    Q 05 ts/ js node js of 45 assignment

---
+++ q5.js
@@ -0,0 +1,5 @@
+//Q5Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose
+//your message and store it in a new variable called message. Print your message.
+var famous_person = "Jim Carrey";
+var message = "Behind every great man is woman rolling her eyes.";
+console.log("".concat(famous_person, " once said,").concat(message));
